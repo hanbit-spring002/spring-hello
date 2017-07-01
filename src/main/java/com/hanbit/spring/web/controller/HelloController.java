90560d73499578bbf48a9460400edd77c00e9a2b
hb4000 <hb4000@hb4000-PC>
2017-07-01 18:36:16 +0900

    add json api

--- src/main/java/com/hanbit/spring/web/controller/HelloController.java
+++ src/main/java/com/hanbit/spring/web/controller/HelloController.java
@@ -1,7 +1,11 @@
 package com.hanbit.spring.web.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -11,4 +15,23 @@ public class HelloController {
 		return "hello"; //     WEB-INF/jsp/hello.jsp
 	}
 	
+	@RequestMapping("/json")
+	@ResponseBody
+	public Map json() {
+		Map map = new HashMap();
+		map.put("name", "Hanbit");
+		map.put("data", true);
+		
+		return map;
+	}
+	
 }
+
+
+
+
+
+
+
+
+
